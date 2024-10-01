c13c378453719accc5f471059da6cb3599a28256
yahialm <yahia.lamhafad@aiac.ma>
2024-10-01 08:24:48 +0100

    Update: New aliases added to the command.go file

--- cmd/command.go
+++ cmd/command.go
@@ -8,6 +8,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+
+/*Run the developement server*/
 var serverCmd = &cobra.Command{
     Use:   "server",
     Short: "Run the Django development server",
@@ -28,6 +30,9 @@ func runserver() {
     }
 }
 
+
+
+/*Applies migrations to the database*/
 var migCmd = &cobra.Command{
     Use:   "mig",
     Short: "Run python manage.py migrate",
@@ -48,16 +53,18 @@ func migrate() {
     }
 }
 
+
+/*Creates new migrations based on the changes detected in models*/
 var mkmigCmd = &cobra.Command{
     Use:   "mkmig",
-    Short: "Run the Django development server",
+    Short: "Run python manage.py makemigrations",
     Run: func(cmd *cobra.Command, args []string) {
         makemigrations()
     },
 }
 
 func makemigrations() {
-    cmd := exec.Command("cmd", "/C", "env\\Scripts\\python.exe manage.py migrate")
+    cmd := exec.Command("cmd", "/C", "env\\Scripts\\python.exe manage.py makemigrations")
 
     // Attach the process to the std output and error
     cmd.Stdout = os.Stdout
@@ -68,16 +75,106 @@ func makemigrations() {
 }
 
 
-// var appCmd = &cobra.Command{
-//     Use:   "app",
-//     Short: "Run the Django development server",
-//     Run: func(cmd *cobra.Command, args []string) {
-//         // Execute: python manage.py runserver
-//         out, err := exec.Command("python", "manage.py", "runserver").Output()
-//         if err != nil {
-//             fmt.Println("Error:", err)
-//             return
-//         }
-//         fmt.Println(string(out))
-//     },
-// }
+/*Lists all migrations and their status*/
+var shmigCmd = &cobra.Command{
+    Use:   "shmig",
+    Short: "Run python manage.py showmigrations",
+    Run: func(cmd *cobra.Command, args []string) {
+        showmigrations()
+    },
+}
+
+func showmigrations() {
+    cmd := exec.Command("cmd", "/C", "env\\Scripts\\python.exe manage.py showmigrations")
+
+    // Attach the process to the std output and error
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    if err := cmd.Run(); err != nil {
+        fmt.Println("--------------------------------------------")
+    }
+}
+
+
+
+/*Opens the database shell for direct database access*/
+var dbshCmd = &cobra.Command{
+    Use:   "dbsh",
+    Short: "Run python manage.py dbshell",
+    Run: func(cmd *cobra.Command, args []string) {
+        dbshell()
+    },
+}
+
+func dbshell() {
+    cmd := exec.Command("cmd", "/C", "env\\Scripts\\python.exe manage.py dbshell")
+
+    // Attach the process to the std output and error
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    if err := cmd.Run(); err != nil {
+        fmt.Println("--------------------------------------------")
+    }
+}
+
+
+/*Opens the interactive Python shell with Django context*/
+var shCmd = &cobra.Command{
+    Use:   "sh",
+    Short: "Run python manage.py shell",
+    Run: func(cmd *cobra.Command, args []string) {
+        shell()
+    },
+}
+
+func shell() {
+    cmd := exec.Command("cmd", "/C", "env\\Scripts\\python.exe manage.py shell")
+
+    // Attach the process to the std output and error
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    if err := cmd.Run(); err != nil {
+        fmt.Println("--------------------------------------------")
+    }
+}
+
+/*Run tests*/
+var testCmd = &cobra.Command{
+    Use:   "test",
+    Short: "Run python manage.py test",
+    Run: func(cmd *cobra.Command, args []string) {
+        test()
+    },
+}
+
+func test() {
+    cmd := exec.Command("cmd", "/C", "env\\Scripts\\python.exe manage.py test")
+
+    // Attach the process to the std output and error
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    if err := cmd.Run(); err != nil {
+        fmt.Println("--------------------------------------------")
+    }
+}
+
+
+/*Create super*/
+var suCmd = &cobra.Command{
+    Use:   "su",
+    Short: "Run python manage.py createsuperuser",
+    Run: func(cmd *cobra.Command, args []string) {
+        createsu()
+    },
+}
+
+func createsu() {
+    cmd := exec.Command("cmd", "/C", "env\\Scripts\\python.exe manage.py createsuperuser")
+
+    // Attach the process to the std output and error
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    if err := cmd.Run(); err != nil {
+        fmt.Println("--------------------------------------------")
+    }
+}
--- cmd/root.go
+++ cmd/root.go
@@ -23,8 +23,13 @@ func Execute() {
 func init() {
     // Add subcommands here
     rootCmd.AddCommand(serverCmd) // serve
-    rootCmd.AddCommand(setupCmd) // setup
-    rootCmd.AddCommand(appCmd)  // Create app - add app to INSTALLED_APPS in setting.py
-    rootCmd.AddCommand(migCmd)
-    rootCmd.AddCommand(mkmigCmd)
+    rootCmd.AddCommand(setupCmd)  // setup
+    rootCmd.AddCommand(appCmd)    // Create app - add app to INSTALLED_APPS in setting.py
+    rootCmd.AddCommand(migCmd)    // migrate
+    rootCmd.AddCommand(mkmigCmd)  // makemigrations
+    rootCmd.AddCommand(shmigCmd)  // showmigrations
+    rootCmd.AddCommand(dbshCmd)   // dbshell
+    rootCmd.AddCommand(shCmd)     // shell
+    rootCmd.AddCommand(testCmd)   // test
+    rootCmd.AddCommand(suCmd)     // createsuperuser
 }
